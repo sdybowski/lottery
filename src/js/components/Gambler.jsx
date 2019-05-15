d7805bc6e9fcf61cde85b4df032e661b5f5996a1
sebdybowski <dybowski@int.pl>
2019-05-16 01:21:16 +0200

    brief gambler addition

---
+++ src/js/components/Gambler.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import _ from 'lodash';
+
+export const Gambler = ({ mode, draw }) => {
+	return <section>
+		<button onClick={draw}>Draw</button>
+		<p>
+			<span>ddd</span>
+		</p>
+	</section>
+};
--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
 import { ROUTES } from '../app/routing';
+import { Gambler } from '../components/Gambler';
 
 const HomeComponent = ({ goToTest }) => <div>
 	<h1>Home</h1>
 	<button onClick={goToTest}>Go to Test</button>
+	<hr />
+	<Gambler />
 </div>;
 
 const mapStateToProps = state => ({});
